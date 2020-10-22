0c513cced000431c503ce697f06edde00a5cae94
Sp <41648020+TheoJSP@users.noreply.github.com>
2020-10-22 20:07:08 -0300

    About + correciones en el navbar

--- app.js
+++ app.js
@@ -6,17 +6,5 @@ $(document).ready(()=>{
         }else{
             $(".navbar").removeClass("sticky")
         }
-
-        if(this.scrollY > 500){
-            $(".scroll-up-btn").addClass("show")
-        }else{
-            $(".scroll-up-btn").removeClass("show")
-        }
-    
-
     });
-
-    
-
-
 })
